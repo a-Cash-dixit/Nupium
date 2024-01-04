f67065885d68c01542d7c15793a92cc6f221a5db
Akash Dixit <83697333+a-Cash-dixit@users.noreply.github.com>
2024-01-04 23:00:36 +0530

    4th commit

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -2,6 +2,7 @@ import Features from "./components/Features/Features.jsx";
 import Navbar from "./components/Navbar/Navbar";
 import ProcessFlow from "./components/Process/ProcessFlow.jsx";
 import Slides from "./components/Slides/Slides";
+import Web3 from "./components/Web3/Web3.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Slides />
       <Features />
       <ProcessFlow />
+      <Web3 />
     </>
   );
 }
---
+++ landingpage/src/components/Web3/Web3.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Web3.css";
+const Web3 = () => {
+  return (
+    <div className="Web3">
+      <h1>Degree in Web 3 Technologies (Two Year Programme)</h1>
+      <p>
+        This degree program aims to provide a comprehensive understanding of Web
+        3 technology and its intersection with other emerging technologies such
+        as Blockchain, IoT, Artificial Intelligence, and Machine Learning. Each
+        semester focuses on a different technological field, culminating in a
+        well-rounded, interdisciplinary education.
+      </p>
+      <ul>
+        <li>An intensive, holistic program that spans over 24 months.</li>
+        <li>
+          Dive deep into Web3, AI, Blockchain, Machine Learning and IoT with a
+          curriculum that's both comprehensive and cutting-edge.
+        </li>
+        <li>
+          Upon completion, you'll be awarded a degree that stands as a testament
+          to your expertise and readiness for the industry.
+        </li>
+        <li>Total 144 Hour Live Classes in 2 year duration.</li>
+        <li>4 Semester in duration of two years.</li>
+        <li>
+          Each Semester consists of 3 Courses, that means 12 different courses
+          will be there in the syllabus.
+        </li>
+        <li>6 Hour each course divided in 2 classes every month.</li>
+      </ul>
+    </div>
+  );
+};
+export default Web3;
