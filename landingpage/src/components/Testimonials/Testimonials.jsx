5ba3fba2140bc7df1ae78551f116299dc27f4626
Akash Dixit <83697333+a-Cash-dixit@users.noreply.github.com>
2024-02-02 21:38:22 +0530

    2feb

--- landingpage/src/components/Testimonials/Testimonials.jsx
+++ landingpage/src/components/Testimonials/Testimonials.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import TestimonialCard from "./TestimonialCard";
 import "./Testimonials.css";
-const Testimonials=()=>{
-    return (
-        <div className="Testimonials">
-            <div className="heading">
-                TESTIMONIALS
-            </div>
-            <div className="Cards">
-            <TestimonialCard />
-            <TestimonialCard />
-            <TestimonialCard />
-            <TestimonialCard />
-            </div>
+const Testimonials = () => {
+  return (
+    <div className="Testimonials">
+      <div className="heading">TESTIMONIALS</div>
+      <div className="Cards">
+          <TestimonialCard />
+          <TestimonialCard />
+          <TestimonialCard />
+          <TestimonialCard />
         </div>
-    )
-}
+      </div>
+  );
+};
 export default Testimonials;
